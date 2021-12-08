99505e09e5f58632b3ea08a589addcc1466461f2
jiahoe <mrjiahoe@hotmail.com>
2021-12-08 16:21:23 +0800

    add user field

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import restProvider from "ra-data-simple-rest";
 import { PostList } from "./components/PostList";
 import { PostCreate } from "./components/PostCreate";
 import { PostEdit } from "./components/PostEdit";
+import { UserList } from "./components/PostList";
+import { UserCreate } from "./components/PostCreate";
+import { UserEdit } from "./components/PostEdit";
 
 function App() {
 	return (
@@ -12,6 +15,12 @@ function App() {
 				list={PostList}
 				create={PostCreate}
 				edit={PostEdit}
+			/>
+			<Resource
+				name="user"
+				list={UserList}
+				create={UserCreate}
+				edit={UserEdit}
 			/>
 		</Admin>
 	);
